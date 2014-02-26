2848585d7520f77e3f3b184b0ac29023ef29820b
jnpost <jnpost@email.arizona.edu>
2014-02-25 18:17:52 -0700

    HW3 first outline

---
+++ HW3/HW3.py
@@ -0,0 +1,89 @@
+"""Authors: Margaret Preston
+                     Jay Post
+Date: 2/24/14
+Class: ISTA 350
+Section Leader: logan
+
+Description: We are creating a subclass that can modify input in multiple different ways."""
+
+class MyString(str):
+    def _encode(self, to_encode):
+        lst = []
+        for i in to_encode:
+            lst.append(self.to_encode.pop())
+        lst.reverse()
+        if lst.isdigit():
+            for item in lst:
+                encoded.append(chr(127-ord(lst[item]))
+        else:
+            for item in lst:
+                encoded.append(ord(127-chr(lst[item]))
+
+    def encrypt(self):
+
+    def is_palindrome(self):
+        if self == self.reverse():
+            return True
+        else:
+            return False
+
+    def longest_palindome(self):
+
+    def banner(self, lines=1, fill=''):
+
+    def square_banner(self, size = 3, fill = ''):
+
+    def musical_chairs(self):
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+    def encrypt(self):
+
+
+    def is_palindrome(self):
+
+
+    def longest_palindome(self):
+
+
+    def banner(self, lines=1, fill=''):
+
+
+    def square_banner(self, size = 3, fill = ''):
+
+
+    def musical_chairs(self):
+
+
+if __name__ == '__main__':
+    main()
+
+        else:
+            for item in lst:
+
+
+
+    def encrypt(self):
+
+
+    def is_palindrome(self):
+
+
+    def longest_palindome(self):
+
+
+    def banner(self, lines=1, fill=''):
+
+
+    def square_banner(self, size = 3, fill = ''):
+
+
+    def musical_chairs(self):
+
+
+if __name__ == '__main__':
+    main()
